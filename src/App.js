abe3bf460b59bd5fa3d4a917b0bb220fc501cd2d
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-10-10 13:22:57 -0700

    links added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,29 @@ import { sRGBEncoding } from 'three';
 
 function App(props) {
 
+   const links = [
+      { name: 'Ready Player Me', src: '' },
+      { name: 'Blender', src: '' },
+      { name: 'Mixamo', src: '' }
+   ]
+
+   const rendered = links.map((link) => 
+        <a href={link.src} target="_blank" children={link.name} />
+   )
+
   return (
    <>
-      {/* <h1 style={{
-         margin: '0',
-         padding: '1em',
+      <h1 style={{
+         padding: '1em 0',
          fontSize: '4em',
          textAlign: 'center',
-      }}>{props.name} in 3D with Three.js</h1> */}
+         alignSelf: 'center',
+         position: 'fixed',
+         minWidth: '100vw'
+      }}>{props.name} in 3D with Three.js</h1>
+      <div style={{display: 'flex', padding: '3em 0', width: '100%', position: 'absolute', bottom: '0', justifyContent: 'space-around'}}>
+         {rendered}
+      </div>
    
       <Canvas
       camera={{ position: [0, 0, 12.25], fov: 15 }}
